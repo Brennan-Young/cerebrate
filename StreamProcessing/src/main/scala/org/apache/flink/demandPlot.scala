ffad5bf01bd5a0ce89aec42c83125e1caf634142
Brennan-Young <m.brennanyoung@gmail.com>
2017-02-07 23:47:48 +0000

    Cleaned up files and added comments

--- StreamProcessing/src/main/scala/org/apache/flink/demandPlot.scala
+++ StreamProcessing/src/main/scala/org/apache/flink/demandPlot.scala
@@ -32,12 +32,18 @@ class TimestampExtractor extends AssignerWithPeriodicWatermarks[String] with Ser
 
 
   def main(args: Array[String]) {
-    // getNetworkInfo.readFile
+
+
     val graph = getNetworkInfo.readFile
     val v = graph.getVertices
     v.print
+
+
+    // establish streaming environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+    // get streamed data from Kafka
     val properties = new Properties()
     properties.setProperty("bootstrap.servers", "ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092")
     properties.setProperty("zookeeper.connect", "ec2-52-33-229-60.us-west-2.compute.amazonaws.com:2181")
@@ -46,24 +52,32 @@ class TimestampExtractor extends AssignerWithPeriodicWatermarks[String] with Ser
     val stream = env
       .addSource(new FlinkKafkaConsumer09[String]("my-topic", new SimpleStringSchema(), properties))
       .assignTimestampsAndWatermarks(new TimestampExtractor)
+
 /*
 The next line is important for anyone trying to learn Scala and Flink at the same time.  TODO Explain why it's important
 */
+    // split input string on spaces, turn into tuple with the number 1 appended
     val parsedStream = stream.map(value => value.split("\\s+") match { case Array(x,y) => (x,y.toLong,1) })
-//  TODO compute sliding window counts
-    val requestCounts = parsedStream.keyBy(0).fold("0", 0L, 0)((s: (String, Long, Int), r: (String, Long, Int)) => { (r._1, s._2.max(r._2), s._3 + r._3) } )
- val packedStream = requestCounts.map(value => value.toString())
-//   packedStream.print
+    // key on node ID and record running counts of requests
+    val requestCounts = parsedStream.keyBy(0)
+        .fold("0", 0L, 0)((s: (String, Long, Int), r: (String, Long, Int)) => { (r._1, s._2.max(r._2), s._3 + r._3) } )
+    // pack back into string for Kafka
+    val packedStream = requestCounts.map(value => value.toString())
+    // packedStream.print
+    // Kafka sink
     packedStream.addSink(new FlinkKafkaProducer09[String]("ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092", "flink-to-kafka", new SimpleStringSchema()))
 
-// Windowed Counting
-//   val windowedCount = stream.map{(m: String) => (m.split(" ")(0)(1), 1) }
-     val windowedCount = stream.map(value => value.split("\\s+") match { case Array(x,y) => (x,y.toLong,1)} )
+    // Windowed Counting
+    /* 
+    split input string on spaces, turn into tuple with the number 1 appended.  Key on node ID.  Count all requests in a key that happened in the last 2 seconds, with the window advancing by 1 second.
+    */
+    // val windowedCount = stream.map{(m: String) => (m.split(" ")(0)(1), 1) }
+    val windowedCount = stream.map(value => value.split("\\s+") match { case Array(x,y) => (x,y.toLong,1)} )
 				.keyBy(0)
 				.timeWindow(Time.milliseconds(2000), Time.milliseconds(1000))
 				.sum(2)
-//				.map(value => (value._2)/2000)
-     windowedCount.map(value => value.toString()).addSink(new FlinkKafkaProducer09[String]("ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092", "demand-plots", new SimpleStringSchema()))
+    // pack into string for Kafka and add a sink
+    windowedCount.map(value => value.toString()).addSink(new FlinkKafkaProducer09[String]("ec2-52-33-229-60.us-west-2.compute.amazonaws.com:9092", "demand-plots", new SimpleStringSchema()))
 
     env.execute("Flink Kafka Example")
   }
--- StreamProcessing/src/main/scala/org/apache/flink/getNetworkInfo.scala
+++ StreamProcessing/src/main/scala/org/apache/flink/getNetworkInfo.scala
@@ -10,15 +10,25 @@ import org.apache.flink.graph.scala.utils.Tuple3ToEdgeMap
 
 object getNetworkInfo { 
 
-def printdim(buffer: ArrayBuffer[Int]) = println(buffer mkString "x")
-
 def readFile() : Graph[String, Long, Double] = {
 
+/*
+Reads information about the network from Hadoop Distributed File System.  Places this network information into a graph and returns the graph data object. The network information comes in two csv files:
 
+supplies.csv: a comma separated values sequence, where the first field is the node (sensor) ID and the second field is the initial supply at that sensor.
+
+network.csv: a comma separated values sequence, where the first field is the ID of the source node, the second field is the ID of the target node, and the third field is the distance between these (the edge weight)
+
+*/
     
 	val env = ExecutionEnvironment.getExecutionEnvironment
 
-
+	/* 
+	String: vertex ID type
+	Long: vertex attribute type
+	Double: edge attribute type
+	edge source/target types are implicitly string as the vertex ID types are strings
+	*/
     val graph = Graph.fromCsvReader[String, Long, Double](
 		pathVertices = "hdfs://ec2-52-33-88-184.us-west-2.compute.amazonaws.com:9000/test/supplies.csv",
 		pathEdges = "hdfs://ec2-52-33-88-184.us-west-2.compute.amazonaws.com:9000/test/network.csv",
@@ -28,12 +38,6 @@ def readFile() : Graph[String, Long, Double] = {
 	return graph
 
 }
-
-
-
-
-
-
 
 
 }
