c28ec26c9750d964669e4f1bd581d452cb4725b3
Brennan-Young <m.brennanyoung@gmail.com>
2017-02-15 22:36:55 -0800

    Directory cleanup

--- networkGenToFlink.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ~/.profile
-
-python networkGen.py
-scp -i ~/.ssh/brennan-young.pem network.csv ubuntu@ec2-52-33-88-184.us-west-2.compute.amazonaws.com:~/Cerebrate/cerebrate/StreamProcessing/src/main/resources
-scp -i ~/.ssh/brennan-young.pem supplies.csv ubuntu@ec2-52-33-88-184.us-west-2.compute.amazonaws.com:~/Cerebrate/cerebrate/StreamProcessing/src/main/resources
